26660a35c233687ed263947f1e4bc2de65053970
JonathanBook <59934702+JonathanBook@users.noreply.github.com>
2020-06-23 23:30:19 +0400

    First update

---
+++ js/move.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+    anchors: ['page1', 'page2', 'page3', 'page4']
+    
+  });
+  
+/*   //adding the action to the button
+  $(document).on('click', '#silentMoveTo', function(){
+    fullpage_api.silentMoveTo('page2', 2);
+  }); */
