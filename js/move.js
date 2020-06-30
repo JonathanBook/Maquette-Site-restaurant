6d6f593e0be1ac3863cfd0cc58bea8a0b368eb3f
JonathanBook <59934702+JonathanBook@users.noreply.github.com>
2020-06-30 15:30:30 +0400

    ddfdf

--- js/move.js
+++ js/move.js
@@ -1,9 +1,49 @@
-new fullpage('#fullpage', {
+
+var ratio = 0.3
+var options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+$(document).ready(function () {
+  $('.main-menu').fadeOut();
+  $('#fullpage').fullpage({
     anchors: ['page1', 'page2', 'page3', 'page4']
-    
+
+
+
   });
-  
-/*   //adding the action to the button
-  $(document).on('click', '#silentMoveTo', function(){
-    fullpage_api.silentMoveTo('page2', 2);
-  }); */
+  $('.btn-menu').click(function () {
+    $('.main-menu').fadeIn()
+
+    $('.btn-menu').fadeOut();
+  });
+
+  $('.Croix').click(function () {
+    $('.btn-menu').fadeIn();
+    $('.main-menu').fadeOut();
+  });
+  fullpage_api.setResponsive(false);
+
+
+
+  var isvisible = function (entries, observer) {
+    entries.forEach(function (entry) {
+
+      /*console.log('je te voie')  */
+      if (entry.target.paused) {
+        entry.target.play()
+      }
+
+
+    });
+
+  }
+
+
+  var observer = new IntersectionObserver(isvisible, options);
+  document.querySelectorAll('video').forEach(function (info) {
+    observer.observe(info)
+  })
+});
